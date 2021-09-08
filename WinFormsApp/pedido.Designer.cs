11d6c8c21866b30c11714ce4e006cdb67b898d8a
Artaza_Carlos <carlos.unt08@gmail.com>
2021-09-08 15:24:42 -0300

    buscando una interfaz con WinForms

---
+++ WinFormsApp/pedido.Designer.cs
@@ -0,0 +1,189 @@
+﻿
+namespace WinFormsApp
+{
+    partial class pedido
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNom = new System.Windows.Forms.TextBox();
+            this.txtDir = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTel = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.lbCliente = new System.Windows.Forms.Label();
+            this.txtObs = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre";
+            // 
+            // txtNom
+            // 
+            this.txtNom.Location = new System.Drawing.Point(93, 70);
+            this.txtNom.Name = "txtNom";
+            this.txtNom.Size = new System.Drawing.Size(351, 23);
+            this.txtNom.TabIndex = 1;
+            // 
+            // txtDir
+            // 
+            this.txtDir.Location = new System.Drawing.Point(93, 99);
+            this.txtDir.Name = "txtDir";
+            this.txtDir.Size = new System.Drawing.Size(351, 23);
+            this.txtDir.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Dirección";
+            // 
+            // txtTel
+            // 
+            this.txtTel.Location = new System.Drawing.Point(93, 128);
+            this.txtTel.Name = "txtTel";
+            this.txtTel.Size = new System.Drawing.Size(351, 23);
+            this.txtTel.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 131);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Telefono";
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.Location = new System.Drawing.Point(6, 15);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(100, 28);
+            this.btnAceptar.TabIndex = 6;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            this.btnAceptar.Click += new System.EventHandler(this.btnAceptar_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(113, 15);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(100, 28);
+            this.btnLimpiar.TabIndex = 7;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            // 
+            // lbCliente
+            // 
+            this.lbCliente.AutoSize = true;
+            this.lbCliente.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lbCliente.Location = new System.Drawing.Point(23, 9);
+            this.lbCliente.Name = "lbCliente";
+            this.lbCliente.Size = new System.Drawing.Size(74, 25);
+            this.lbCliente.TabIndex = 8;
+            this.lbCliente.Text = "Pedido";
+            // 
+            // txtObs
+            // 
+            this.txtObs.Location = new System.Drawing.Point(113, 41);
+            this.txtObs.Name = "txtObs";
+            this.txtObs.Size = new System.Drawing.Size(331, 23);
+            this.txtObs.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(23, 44);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(84, 15);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Observaciones";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnLimpiar);
+            this.groupBox1.Controls.Add(this.btnAceptar);
+            this.groupBox1.Location = new System.Drawing.Point(276, 221);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(219, 49);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            // 
+            // pedido
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(507, 272);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtObs);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lbCliente);
+            this.Controls.Add(this.txtTel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtDir);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNom);
+            this.Controls.Add(this.label1);
+            this.Name = "pedido";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNom;
+        private System.Windows.Forms.TextBox txtDir;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Label lbCliente;
+        private System.Windows.Forms.TextBox txtObs;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
